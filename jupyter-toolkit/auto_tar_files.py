5f5bb2500aa1228d14b4d0a2b22b2bfda1caf485
ppj <pengju.pan@nio.com>
2021-11-03 18:46:55 +0800

    init

---
+++ jupyter-toolkit/auto_tar_files.py
@@ -0,0 +1,22 @@
+# 在jupyter中打包相关的文件，然后方便统一下载。
+import os
+import tarfile
+
+def recursive_files(dir_name='.', ignore=None):
+    for dir_name,subdirs,files in os.walk(dir_name):
+        if ignore and os.path.basename(dir_name) in ignore:
+            continue
+
+        for file_name in files:
+            if ignore and file_name in ignore:
+                continue
+
+            yield os.path.join(dir_name, file_name)
+
+def make_tar_file(dir_name='.', tar_file_name='tarfile.tar', ignore=None):
+    tar = tarfile.open(tar_file_name, 'w')
+
+    for file_name in recursive_files(dir_name, ignore):
+        tar.add(file_name)
+
+    tar.close()
